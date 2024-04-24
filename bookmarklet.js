b306a9e1edabd47e6059588a38cc9938a7380445
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2024-04-24 18:28:41 +0100

    Update bookmarklet.js - Instruction

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,9 @@
 javascript:(function(){fetch('https://raw.githubusercontent.com/Ariakim-Taiyo/GeoFs-737-Immersion-SFX/main/index.js').then(response => response.text()).then(code => eval(code));})();
+
+// How to use
+// 1. Create a bookmark and name it whatever you want, in the URL box, paste this script in.
+// 2. Go into GeoFS and select the 737-700, once it loaded, press the bookmark once.
+// 3. If you switched to another aircraft and back to the 737, you will need to press the bookmark again.
+
+// Plugin made by Ariakim-Taiyo
+// Bookmarklet made by Radioactive Potato (krunchiekrunch)
