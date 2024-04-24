c5f85c4bf66227bf89e06d845ade41b7b0d00f7e
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2024-04-24 17:55:46 +0100

    Create bookmarklet.js
    Bookmarklet version

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){fetch('https://raw.githubusercontent.com/Ariakim-Taiyo/GeoFs-737-Immersion-SFX/main/index.js').then(response => response.text()).then(code => eval(code));})();
